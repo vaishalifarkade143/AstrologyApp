7225c20bf3ddc9986b9e5cf1b6adf8094ff35d6d
vaishalifarkade143 <vaishalifarkade143@gmail.com>
2024-06-08 12:28:40 +0530

    second commit

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,21 +1,3 @@
-// src/screens/HomeScreen.js
-// import * as React from 'react';
-// import { View, Text, StyleSheet } from 'react-native';
-// import { Button } from 'react-native-paper';
-
-// const HomeScreen = ({navigation}) => {
-//   return (
-//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-//     <Text>Home Screen</Text>
-//     <Button mode="contained" onPress={() => navigation.navigate('Details')}>
-//       Go to Details
-//     </Button>
-//   </View>
-//   );
-// };
-// export default HomeScreen;
-
-
 
 import * as React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
